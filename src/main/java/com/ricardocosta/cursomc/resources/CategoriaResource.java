be2bcc1c3c12b0531dadb0e2ae6ebb7e92792a05
RFNCosta <r_costa_10@hotmail.com>
2020-07-11 15:25:49 +0100

    Primeira Class de dominio - Categoria

--- src/main/java/com/ricardocosta/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/ricardocosta/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.ricardocosta.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ricardocosta.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -10,9 +15,16 @@ public class CategoriaResource
 {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar()
+	public List<Categoria> listar()
 	{
-		return "Rest está a funcionar !";
+		Categoria catOne = new Categoria(1, "Informática");
+		Categoria catTwo = new Categoria(2, "Informática");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(catOne);
+		lista.add(catTwo);
+		
+		return lista;
 	}
 	
 }
